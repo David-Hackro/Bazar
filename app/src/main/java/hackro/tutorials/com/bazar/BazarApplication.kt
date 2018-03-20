fc346ea049db577e7a072015c397b327d3f4662c
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2018-03-20 17:52:16 -0600

    add aplication

---
+++ app/src/main/java/hackro/tutorials/com/bazar/BazarApplication.kt
@@ -0,0 +1,15 @@
+package hackro.tutorials.com.bazar
+
+import android.app.Application
+
+/**
+ * Created by macbookpro on 20/03/18.
+ */
+class BazarApplication : Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
