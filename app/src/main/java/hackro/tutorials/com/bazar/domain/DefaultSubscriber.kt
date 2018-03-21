4ca92858db6251b4e805d9238931c2eb0323626f
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2018-03-20 19:05:15 -0600

    add layer domain

---
+++ app/src/main/java/hackro/tutorials/com/bazar/domain/DefaultSubscriber.kt
@@ -0,0 +1,10 @@
+package hackro.tutorials.com.bazar.domain
+
+
+class DefaultSubscriber<T> : rx.Subscriber<T>() {
+    override fun onCompleted() {}
+
+    override fun onError(e: Throwable) {}
+
+    override fun onNext(t: T) {}
+}
---
+++ app/src/main/java/hackro/tutorials/com/bazar/domain/usecase/GetPostFB.kt
@@ -0,0 +1,22 @@
+package hackro.tutorials.com.bazar.domain.usecase
+
+import android.support.annotation.NonNull
+import hackro.tutorials.com.bazar.data.repository.AppRepository
+import rx.Observable
+import javax.inject.Inject
+
+/**
+ * Created by macbookpro on 20/03/18.
+ */
+class GetPostFB {
+
+    private lateinit var appRepository : AppRepository
+
+
+    @Inject constructor(@NonNull appRepository: AppRepository) {
+        this.appRepository = appRepository
+    }
+
+    fun buildObservableUseCase(): Observable<*> {return appRepository.getPostsFB() }
+
+}
